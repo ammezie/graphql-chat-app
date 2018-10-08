3f979c7c1f24bfa50be443112e038fc1303974ef
Chimezie Enyinnaya <meziemichael@gmail.com>
2018-10-08 09:52:27 +0100

    configure vue apollo

---
+++ frontend/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app" class="container" style="padding-top: 100px">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-body">
+
+            <div class="row">
+              <div class="col-md-12">
+                <form method="post" @submit.prevent="enterChat">
+                  <div class="form-group">
+                    <div class='input-group'>
+                      <input type='text' class="form-control" v-model="username" placeholder="Enter your username">
+
+                      <div class='input-group-append'>
+                        <button class='btn btn-primary'>Enter</button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12">
+                <div class="card">
+                  <div class="card-header">Chatbox</div>
+                  <div class="card-body">
+                    <dl>
+                      <dt>mezie</dt>
+                      <dd>Hi</dd>
+                      <dt>bode</dt>
+                      <dd>Hey!</dd>
+                    </dl>
+                    <hr>
+                    <form method="post" @submit.prevent="sendMessage">
+                      <div class='input-group'>
+                        <input type='text' class="form-control" v-model="message" placeholder="Type your message...">
+
+                        <div class='input-group-append'>
+                          <button class='btn btn-primary'>Send</button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      username: '',
+      messgage: '',
+    };
+  },
+  methods: {
+    enterChat() {
+      alert(this.username);
+    },
+    sendMessage() {},
+  },
+};
+</script>
---
+++ frontend/src/main.js
@@ -0,0 +1,48 @@
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from 'apollo-client'
+import { split } from 'apollo-link'
+import { HttpLink } from 'apollo-link-http'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+import Vue from 'vue'
+import VueApollo from 'vue-apollo'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000'
+})
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4000',
+  options: {
+    reconnect: true
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink
+)
+
+const apolloClient = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
+
+Vue.use(VueApollo)
+
+new Vue({
+  apolloProvider,
+  render: h => h(App)
+}).$mount('#app')
